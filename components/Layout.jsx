9300d810d0fa955c8b21d2c2648c47a085f34999
hernan066 <hernan0066@gmail.com>
2022-01-12 03:54:05 -0300

    Products

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,36 +1,30 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
-import Head from 'next/head'
-import React from 'react'
-import useStyles from '../utils/styles'
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import Head from "next/head";
+import React from "react";
+import useStyles from "../utils/styles";
 
-const Layout = ({children}) => {
-    
-    const classes = useStyles();
+const Layout = ({ children }) => {
+  const classes = useStyles();
 
-    return (
-        <div>
-            <Head>
-                <title>Next Shop</title>
-            </Head>
-            <AppBar position="static" className={classes.navbar}>
-                <Toolbar>
-                    <Typography>Next Shop</Typography>
-                </Toolbar>
-                
-            </AppBar>
-            <Container className={classes.main}>
-                {children}
-            </Container>
+  return (
+    <div>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography>Next Shop</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
 
-            <footer className={classes.footer}>
-                <Typography>
-                 All rights reserved &copy; {new Date().getFullYear()} - Hernan Moneta
-                </Typography>
+      <footer className={classes.footer}>
+        <Typography>
+          All rights reserved &copy; {new Date().getFullYear()} - Hernan Moneta
+        </Typography>
+      </footer>
+    </div>
+  );
+};
 
-            </footer>
-
-        </div>
-    )
-}
-
-export default Layout
+export default Layout;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,43 @@
-import { Divider } from "@material-ui/core";
-import Head from "next/head";
-import Image from "next/image";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import Layout from "../components/Layout";
-import styles from "../styles/Home.module.css";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product1</li>
-          <li>Product2</li>
-          <li>Product3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">Add to cart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   );
