f35b1d3bf7e2ae5fa81ebff90f36dcbcce618c8d
hernangit config --global user.name John <hernan0066@gmail.com>
2022-01-16 17:54:36 -0300

    Fetch Products-From-API

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,14 +11,19 @@ import {
 import Layout from "../components/Layout";
 import data from "../utils/data";
 import NextLink from "next/link";
+import db from "../utils/db";
+import Product from '../models/Product';
 
-export default function Home() {
+export default function Home(props) {
+  
+  const { products } = props;
+  
   return (
     <Layout>
       <div>
         <h1>Products</h1>
         <Grid container spacing={3}>
-          {data.products.map((product) => (
+          {products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
                 <NextLink href={`/product/${product.slug}`} passHref>
@@ -46,4 +51,15 @@ export default function Home() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(){
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props:{
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
