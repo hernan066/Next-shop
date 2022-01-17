c344235f8a537c7bec9af0d4820629dffa6b350c
hernangit config --global user.name John <hernan0066@gmail.com>
2022-01-17 17:05:04 -0300

    Login-Screen

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import dynamic from 'next/dynamic';
-import Layout from '../components/Layout';
-import { Store } from '../utils/Store';
-import NextLink from 'next/link';
-import Image from 'next/image';
+import React, { useContext } from "react";
+import dynamic from "next/dynamic";
+import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
+import NextLink from "next/link";
+import Image from "next/image";
 import {
   Grid,
   TableContainer,
@@ -20,25 +20,37 @@ import {
   Card,
   List,
   ListItem,
-} from '@material-ui/core';
-import axios from 'axios';
+} from "@material-ui/core";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 function CartScreen() {
   const { state, dispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+  const router = useRouter();
+
+  //update item cart
   const updateCartHandler = async (item, quantity) => {
     const { data } = await axios.get(`/api/products/${item._id}`);
     if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
+      window.alert("Sorry. Product is out of stock");
       return;
     }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
   };
+
+  //remove item cart
   const removeItemHandler = (item) => {
-    dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+    dispatch({ type: "CART_REMOVE_ITEM", payload: item });
   };
+
+  //checkout
+  const checkoutHandler = () => {
+    router.push("/shipping");
+  };
+
   return (
     <Layout title="Shopping Cart">
       <Typography component="h1" variant="h1">
@@ -46,7 +58,7 @@ function CartScreen() {
       </Typography>
       {cartItems.length === 0 ? (
         <div>
-          Cart is empty.{' '}
+          Cart is empty.{" "}
           <NextLink href="/" passHref>
             <Link>Go shopping</Link>
           </NextLink>
@@ -123,13 +135,18 @@ function CartScreen() {
               <List>
                 <ListItem>
                   <Typography variant="h2">
-                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{" "}
                     items) : $
                     {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
                   </Typography>
                 </ListItem>
                 <ListItem>
-                  <Button variant="contained" color="primary" fullWidth>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    onClick={checkoutHandler}
+                  >
                     Check Out
                   </Button>
                 </ListItem>
--- utils/store.js
+++ utils/store.js
@@ -1,23 +1,26 @@
-import Cookies from 'js-cookie';
-import { createContext, useReducer } from 'react';
+import Cookies from "js-cookie";
+import { createContext, useReducer } from "react";
 
 export const Store = createContext();
 const initialState = {
-  darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
+  darkMode: Cookies.get("darkMode") === "ON" ? true : false,
   cart: {
-    cartItems: Cookies.get('cartItems')
-      ? JSON.parse(Cookies.get('cartItems'))
+    cartItems: Cookies.get("cartItems")
+      ? JSON.parse(Cookies.get("cartItems"))
       : [],
   },
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'DARK_MODE_ON':
+    
+    case "DARK_MODE_ON":
       return { ...state, darkMode: true };
-    case 'DARK_MODE_OFF':
+
+    case "DARK_MODE_OFF":
       return { ...state, darkMode: false };
-    case 'CART_ADD_ITEM': {
+
+    case "CART_ADD_ITEM": {
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
@@ -27,14 +30,15 @@ function reducer(state, action) {
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
-      Cookies.set('cartItems', JSON.stringify(cartItems));
+      Cookies.set("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
-    case 'CART_REMOVE_ITEM': {
+
+    case "CART_REMOVE_ITEM": {
       const cartItems = state.cart.cartItems.filter(
         (item) => item._id !== action.payload._id
       );
-      Cookies.set('cartItems', JSON.stringify(cartItems));
+      Cookies.set("cartItems", JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
 
