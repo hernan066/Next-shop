551c927125847772d9528a43c38f5d988e9a1e43
hernangit config --global user.name John <hernan0066@gmail.com>
2022-01-17 20:26:35 -0300

    Build-Login-API

--- pages/login.js
+++ pages/login.js
@@ -1,56 +1,76 @@
 import {
-    List,
-    ListItem,
-    Typography,
-    TextField,
-    Button,
-    Link,
-  } from '@material-ui/core';
-  import NextLink from 'next/link';
-  import React from 'react';
-  import Layout from '../components/Layout';
-  import useStyles from '../utils/styles';
-  
-  export default function Login() {
-    const classes = useStyles();
-    return (
-      <Layout title="Login">
-        <form className={classes.form}>
-          <Typography component="h1" variant="h1">
-            Login
-          </Typography>
-          <List>
-            <ListItem>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="email"
-                label="Email"
-                inputProps={{ type: 'email' }}
-              ></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="password"
-                label="Password"
-                inputProps={{ type: 'password' }}
-              ></TextField>
-            </ListItem>
-            <ListItem>
-              <Button variant="contained" type="submit" fullWidth color="primary">
-                Login
-              </Button>
-            </ListItem>
-            <ListItem>
-              Don't have an account? &nbsp;
-              <NextLink href="/register" passHref>
-                <Link>Register</Link>
-              </NextLink>
-            </ListItem>
-          </List>
-        </form>
-      </Layout>
-    );
-  }
+  List,
+  ListItem,
+  Typography,
+  TextField,
+  Button,
+  Link,
+} from "@material-ui/core";
+import axios from "axios";
+import NextLink from "next/link";
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import useStyles from "../utils/styles";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/users/login", {
+        email,
+        password,
+      });
+      alert("Successfully logged in!");
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
+
+  return (
+    <Layout title="Login">
+      <form className={classes.form} onSubmit={submitHandler}>
+        <Typography component="h1" variant="h1">
+          Login
+        </Typography>
+        <List>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="email"
+              label="Email"
+              inputProps={{ type: "email" }}
+              onChange={(e) => setEmail(e.target.value)}
+            ></TextField>
+          </ListItem>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="password"
+              label="Password"
+              inputProps={{ type: "password" }}
+              onChange={(e) => setPassword(e.target.value)}
+            ></TextField>
+          </ListItem>
+          <ListItem>
+            <Button variant="contained" type="submit" fullWidth color="primary">
+              Login
+            </Button>
+          </ListItem>
+          <ListItem>
+            Don't have an account? &nbsp;
+            <NextLink href="/register" passHref>
+              <Link>Register</Link>
+            </NextLink>
+          </ListItem>
+        </List>
+      </form>
+    </Layout>
+  );
+}
