4c0d8829b6796c4872db78141007dc6eaf9a38f6
hernangit config --global user.name John <hernan0066@gmail.com>
2022-01-26 21:48:01 -0300

    Create-User-Edit-Page

--- pages/api/admin/users/[id]/index.js
+++ pages/api/admin/users/[id]/index.js
@@ -2,29 +2,20 @@ import nc from 'next-connect';
 import { isAdmin, isAuth } from '../../../../../utils/auth';
 import User from '../../../../../models/User';
 import db from '../../../../../utils/db';
-
 const handler = nc();
 handler.use(isAuth, isAdmin);
-
 handler.get(async (req, res) => {
   await db.connect();
   const user = await User.findById(req.query.id);
   await db.disconnect();
   res.send(user);
 });
-
 handler.put(async (req, res) => {
   await db.connect();
   const user = await User.findById(req.query.id);
   if (user) {
     user.name = req.body.name;
-    user.slug = req.body.slug;
-    user.price = req.body.price;
-    user.category = req.body.category;
-    user.image = req.body.image;
-    user.brand = req.body.brand;
-    user.countInStock = req.body.countInStock;
-    user.description = req.body.description;
+    user.isAdmin = Boolean(req.body.isAdmin);
     await user.save();
     await db.disconnect();
     res.send({ message: 'User Updated Successfully' });
@@ -33,7 +24,6 @@ handler.put(async (req, res) => {
     res.status(404).send({ message: 'User Not Found' });
   }
 });
-
 handler.delete(async (req, res) => {
   await db.connect();
   const user = await User.findById(req.query.id);
@@ -46,5 +36,4 @@ handler.delete(async (req, res) => {
     res.status(404).send({ message: 'User Not Found' });
   }
 });
-
 export default handler;
