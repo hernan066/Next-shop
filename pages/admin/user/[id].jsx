ef1bf0b6ce35ad487144bea32f03b3b77df5e4b1
hernangit config --global user.name John <hernan0066@gmail.com>
2022-02-10 12:03:56 -0300

    Fix2-Eslint

--- pages/admin/user/[id].jsx
+++ pages/admin/user/[id].jsx
@@ -91,6 +91,7 @@ function UserEdit({ params }) {
       };
       fetchData();
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const submitHandler = async ({ name }) => {
